d2f76726b3983df3b373e7847051c2d449e88100
vakson-cpu <vakson12@gmail.com>
2022-11-21 19:46:33 +0100

    Create AuthSlice and implement logic for Login

---
+++ src/Redux/AuthSlice.js
@@ -0,0 +1,43 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { UserService } from "../Services/UserService";
+import { STATUS } from "../Enums/StatusEnum";
+const initialState = {
+  status:STATUS.IDLE,
+  IsLoggedIn:false
+
+};
+
+
+export const LogInUser = createAsyncThunk(
+    "Users/register",
+    async (payload, thunkAPI) => {
+      try {
+        return await UserService.LogIn(payload);
+      } catch (error) {
+        return "ERROR"
+      }
+    }
+  );
+export const AuthSlice = createSlice({
+  name: "AuthSlice",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [LogInUser.rejected]: (state, action) => {
+      state.status = STATUS.FAILED;
+      state.IsLoggedIn=false;
+
+    },
+    [LogInUser.pending]: (state, action) => {
+      state.status = STATUS.PENDING;
+    },
+    [LogInUser.fulfilled]: (state, action) => {
+      state.status = STATUS.SUCCEDED;
+      state.IsLoggedIn=true;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+
+export default AuthSlice.reducer;
