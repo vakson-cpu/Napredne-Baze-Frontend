9359b6bfacbfd4750407531b9fc910fc0e324cc3
vakson-cpu <vakson12@gmail.com>
2022-11-21 19:40:36 +0100

    Add navbar and routing in App.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "react-bootstrap";
+import RegionCard from "./Components/RegionCard/RegionCard";
+import Info from "./Pages/InfoPage/Info";
+import Region from "./Pages/RegionPage/Region";
+import Footer from "./Shared/Footer/Footer";
+import Navbar from "./Shared/Navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Info />}></Route>
+        <Route path="/Regions" element={<Region />} />
+      </Routes>
+      <Footer></Footer>
+    </>
   );
-}
+};
 
 export default App;
