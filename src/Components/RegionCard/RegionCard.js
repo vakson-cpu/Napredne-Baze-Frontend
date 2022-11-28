ff3069660d7d506fb9667e139abdc4e7a277a18b
vakson-cpu <vakson12@gmail.com>
2022-11-28 03:07:08 +0100

    Create region card js

--- src/Components/RegionCard/RegionCard.js
+++ src/Components/RegionCard/RegionCard.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React,{useEffect} from "react";
 import Card from "react-bootstrap/Card";
 import regionBg from "../.././Assets/RegionCardBg.jpeg";
+import { RegionService } from "../../Services/RegionService";
+import {Button} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
 import './RegionCard.css'
-const RegionCard = () => {
+const RegionCard = ({Name,Area,Villages,Description}) => {
+
+  
   return (
-    <Card className="bg-dark text-white region-card-container">
+    <Card className="mt-5 bg-dark text-white region-card-container">
       <Card.Img className='card-image-bg' src={regionBg} alt="Card image" />
-      <Card.ImgOverlay>
-        <Card.Title>Region Name</Card.Title>
-        <Card.Text>This is a very special region bruh</Card.Text>
-        <Card.Text>Last updated 3 mins ago</Card.Text>
+      <Card.ImgOverlay className='Dark-bg'>
+        <Card.Title>Name of the Region:{<span className='fw-bold fst-italic'>{Name}</span>}</Card.Title>
+        <Card.Text>Area: {<span className='fw-bold fst-italic'>{Area}</span>} km2</Card.Text>
+        <Card.Text>Villages within:{<span className='fw-bold fst-italic'> {Villages}</span>}</Card.Text>
+        <Card.Text>About:{<span className='fw-bold fst-italic'> {Description.slice(0,67)} ...</span>}</Card.Text>
+
+        <Link><Button className='text-center '  variant='outline-danger'>More Info</Button></Link>
       </Card.ImgOverlay>
     </Card>
   );
