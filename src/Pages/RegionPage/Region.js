8d4c09709c99c617f544a0b2fc4c2168eb5f1dde
vakson-cpu <vakson12@gmail.com>
2022-11-28 03:07:37 +0100

    Create Region js

--- src/Pages/RegionPage/Region.js
+++ src/Pages/RegionPage/Region.js
@@ -1,28 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
 import forest from "../.././Assets/Forest1.jpeg";
-import './Region.css'
+import { RegionService } from "../../Services/RegionService";
+import RegionCard from "../../Components/RegionCard/RegionCard";
+import "./Region.css";
+import { Spinner } from "react-bootstrap";
 const Region = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [Regions, setRegions] = useState([]);
+
+  useEffect(() => {
+    const getAllRegions = async () => {
+      let result = await RegionService.GetRegions();
+      setRegions(result);
+      setIsLoading(false);
+    };
+    getAllRegions();
+  }, []);
+  console.log(Regions);
+
   return (
     <>
-    <div className="Region--Banner">
-      <img className="w-100 h-100" src={forest} alt="Banner" />
-    </div>
-    <div className='w-75 m-auto text-center mt-5' >
-      <h1>Welcome to Regions</h1>
-      <p className="mt-5">
-        In order to maintain good organisation of everything, National Park
-        Golija has been divided in regions. Each regions has one or more feeding
-        grounds. If you want to find out more details about our Regions you can
-        take a look bellow.
-      </p>
+      <div className="Region--Banner">
+        <img className="w-100 h-100" src={forest} alt="Banner" />
       </div>
-      <div className="Region-box">
-      
+      <div className="w-75 m-auto text-center mt-5">
+        <h1>Welcome to Regions</h1>
+        <p className="mt-5">
+          In order to maintain good organisation of everything, National Park
+          Golija has been divided in regions. Each regions has one or more
+          feeding grounds. If you want to find out more details about our
+          Regions you can take a look bellow.
+        </p>
       </div>
-
-
-
+      {isLoading === false ? (
+        <div className="Region--box">
+          {Regions.map((item) => (
+            <RegionCard
+              key={item.id}
+              Name={item.name}
+              Area={item.area}
+              Villages={item.villageName}
+              Description={item.description}
+            ></RegionCard>
+          ))}
+        </div>
+      ) : (
+        <div className='text-center mt-5'>
+          <Spinner className='m-auto text-center' animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+      )}
     </>
   );
 };
