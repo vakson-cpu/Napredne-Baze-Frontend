d1c95c6954a449db9ca4695e7957df41ea349fbf
vakson-cpu <vakson12@gmail.com>
2022-12-11 15:15:06 +0100

    Configure CreatePlant service method, add items to formdata

--- src/Services/PlantService.js
+++ src/Services/PlantService.js
@@ -4,21 +4,49 @@ const baseURL = "http://localhost:5000/api";
 axios.defaults.baseURL = baseURL;
 
 export const PlantSrevice = {
-  CreatePlant: async (fileSelected) =>{
-    const formData = new FormData();
-    formData.append("file", fileSelected);
-    console.log(formData);
-    await axios
-      .post("Plants/Create",formData)
-      .then((res) => res)
-      .catch((err) => err)},
-  CreatePlant2: async (fileSelected) =>{
-    const formData = new FormData();
-    formData.append("file", fileSelected);
-    console.log(formData);
-    await axios
-      .post("Plants/ImportFile",formData
-      )
-      .then((res) => res)
-      .catch((err) => err)},
-};
+  CreatePlant: async (plant,file) =>{
+    let bodyFormData= new FormData();
+    bodyFormData.append("file",file);
+    bodyFormData.append("plant",plant)
+    axios({
+      method: "post",
+      url: "Plants/Create",
+      data: bodyFormData,
+      headers: { "Content-Type": "multipart/form-data" },
+    })
+      .then(function (response) {
+        //handle success
+        console.log(response);
+      })
+      .catch(function (response) {
+        //handle error
+        console.log(response);
+      });}
+      ,CreatePlant2: async (plant) =>{
+        console.log(plant)
+        let bodyFormData= new FormData();
+        bodyFormData.append('LatinName',plant.LatinName)
+        bodyFormData.append('LocalName',plant.LocalName)
+        bodyFormData.append('ProtectionId',plant.ProtectionId)
+        bodyFormData.append('Regions',plant.Regions)
+        bodyFormData.append('file',plant.file)
+
+
+        axios({
+          method: "post",
+          url: "Plants/Create",
+          data: bodyFormData,
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+          .then(function (response) {
+            //handle success
+            console.log(response);
+          })
+          .catch(function (response) {
+            //handle error
+            console.log(response);
+          });},
+      
+  };
+
+  
