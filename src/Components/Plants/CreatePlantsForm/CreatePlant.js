332c531e9d157cae8b007fbdc381a62f78953217
vakson-cpu <vakson12@gmail.com>
2022-12-27 12:04:38 +0100

    Configure  logic and design for create plants component.

--- src/Components/Plants/CreatePlantsForm/CreatePlant.js
+++ src/Components/Plants/CreatePlantsForm/CreatePlant.js
@@ -1,10 +1,10 @@
 import React, { useRef, useState, useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { STATUS } from "../../../Enums/StatusEnum";
-import { Spinner } from "react-bootstrap";
+import { Alert, Spinner } from "react-bootstrap";
 import "./CreatePlant.css";
 import { PlantSrevice } from "../../../Services/PlantService";
-
+import { GetRegions } from "../../../Redux/RegionSlice";
 const CreatePlant = () => {
   const filePickerRef = useRef();
 
@@ -19,12 +19,43 @@ const CreatePlant = () => {
   const status = useSelector((state) => state.RegionSlice.status);
   const [checkList, setCheckList] = useState([]);
   const [url, setUrl] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [showToast, setShowToast] = useState(false);
+  const [Toast2, setToast2] = useState(false);
 
+  const dispatch = useDispatch();
+
+  const handleProtections = (Prot) => {
+    console.log(Prot);
+    switch (Prot) {
+      case "L1":
+        return 1;
+      case "L2":
+        return 2;
+      case "L3":
+        return 3;
+      default:
+        return 1;
+    }
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
+    let prot = handleProtections(protection);
+    console.log(checkList);
     // let response=await PlantSrevice.CreatePlant({LatinName:LatinName,LocalName:LocalName,ProtectionId:protection,Regions:checkList},file)
-    let response=await PlantSrevice.CreatePlant2({LatinName:LatinName,LocalName:LocalName,ProtectionId:protection,Regions:checkList,file:file})
-    console.log(response);
+    let response = await PlantSrevice.CreatePlant3({
+      LatinName: LatinName,
+      LocalName: LocalName,
+      ProtectionId: prot,
+      Regions: checkList,
+      file: file,
+    }).finally((res) => setLoading(false));
+    if (response.data === undefined) alert("Action Failed");
+    if (response.data.succeeded) {
+      dispatch(GetRegions());
+      alert("Action Succeeded");
+    }
   };
 
   const handleChecked = (e) => {
@@ -35,7 +66,26 @@ const CreatePlant = () => {
       setCheckList([...helpArray]);
     }
   };
-
+  // <div>
+  //           <CustomToasty
+  //             show={showToast}
+  //             handleShow={setShowToast}
+  //             description="Successfully Added a plant!"
+  //             title="Action succeeded"
+  //             color={"success"}
+  //             textColor={"text-white"}
+  //             position={"start-left"}
+  //           />
+  //           <CustomToasty
+  //             show={Toast2}
+  //             handleShow={setToast2}
+  //             description="Error while adding a plant!"
+  //             title="Action failed"
+  //             color={"danger"}
+  //             textColor={"text-white"}
+  //             position={"start-left"}
+  //           />
+  //         </div>
   useEffect(() => {
     if (!file) return;
     const fileReader = new FileReader();
@@ -48,7 +98,7 @@ const CreatePlant = () => {
   console.log(regions);
   if (status === STATUS.PENDING || status === STATUS.FAILED)
     return (
-      <div className="text-center mt-5">
+      <div className="plant-bg text-center mt-5 mb-5">
         <Spinner
           className="m-auto text-center"
           animation="border"
@@ -60,87 +110,102 @@ const CreatePlant = () => {
     );
   else {
     return (
-      <>
-        {" "}
+      <div className="bg-plant">
         <div id="registration-form">
           <div className="fieldset">
             <legend>Add Plant</legend>
-            <form action="#" method="post" data-validate="parsley">
-              <div className="row">
-                <label>Latin Name</label>
-                <input
-                  type="text"
-                  placeholder="Latin Name"
-                  id="firstname"
-                  data-required="true"
-                  data-error-message="Latin Name is required"
-                  value={LatinName}
-                  onChange={(e) => setLatinName(e.target.value)}
-                />
-              </div>
-              <div className="row">
-                <label for="email">Local Name</label>
-                <input
-                  type="text"
-                  placeholder="LocalName"
-                  name="LocalName"
-                  data-required="true"
-                  data-error-message="Local Name is required"
-                  value={LocalName}
-                  onChange={(e) => setLocalName(e.target.value)}
-                />
-              </div>
-              <div className="row mt-2">
-                <label>Regions</label>
-                {regions.map((item) => {
-                  return (
-                    <div className="regions-box">
-                      <label className="label-checkbox">{item.name}</label>
-                      <input
-                        type="checkbox"
-                        value={item.id}
-                        onChange={handleChecked}
-                      />
-                    </div>
-                  );
-                })}
-              </div>
-              <div className="row mt-3">
-                <label for="protection">Protection Level</label>
-                <select
-                  value={protection}
-                  onChange={(e) => setProtection(e.target.value)}
-                  className="selection-input w-75"
-                >
-                  <option>L1</option>
-                  <option>L2</option>
-                  <option>L3</option>
-                </select>
-              </div>
-              <div className="row">
-                <label>Picture</label>
-                <label onClick={PickImage} className="Special--Label">
-                  Pick a file
-                </label>
-                {url ? <img className="image-box-preview" src={url} alt="nece" /> : "Preview..."}
-                <input
-                  onChange={(e) => setFile(e.target.files[0])}
-                  ref={filePickerRef}
-                  className="image-input"
-                  type="file"
-                  placeholder="Image"
-                  name="image"
-                  data-required="true"
-                  data-type="file"
-                  data-error-message="Image is required"
-                />
-              </div>
+            {loading === false ? (
+              <form action="#" method="post" data-validate="parsley">
+                <div className="row">
+                  <label>Latin Name</label>
+                  <input
+                    type="text"
+                    placeholder="Latin Name"
+                    id="firstname"
+                    data-required="true"
+                    data-error-message="Latin Name is required"
+                    value={LatinName}
+                    onChange={(e) => setLatinName(e.target.value)}
+                  />
+                </div>
+                <div className="row">
+                  <label >Local Name</label>
+                  <input
+                    type="text"
+                    placeholder="LocalName"
+                    name="LocalName"
+                    data-required="true"
+                    data-error-message="Local Name is required"
+                    value={LocalName}
+                    onChange={(e) => setLocalName(e.target.value)}
+                  />
+                </div>
+                <div className="row mt-2">
+                  <label>Regions</label>
+                  {regions.map((item) => {
+                    return (
+                      <div className="regions-box">
+                        <label className="label-checkbox">{item.name}</label>
+                        <input
+                          type="checkbox"
+                          value={item.id}
+                          onChange={handleChecked}
+                        />
+                      </div>
+                    );
+                  })}
+                </div>
+                <div className="row mt-3">
+                  <label >Protection Level</label>
+                  <select
+                    value={protection}
+                    onChange={(e) => setProtection(e.target.value)}
+                    className="selection-input w-75"
+                  >
+                    <option>L1</option>
+                    <option>L2</option>
+                    <option>L3</option>
+                  </select>
+                </div>
+                <div className="row">
+                  <label>Picture</label>
+                  <label onClick={PickImage} className="Special--Label">
+                    Pick a file
+                  </label>
+                  {url ? (
+                    <img className="image-box-preview" src={url} alt="nece" />
+                  ) : (
+                    "Preview..."
+                  )}
+                  <input
+                    onChange={(e) => setFile(e.target.files[0])}
+                    ref={filePickerRef}
+                    className="image-input"
+                    type="file"
+                    placeholder="Image"
+                    name="image"
+                    data-required="true"
+                    data-type="file"
+                    data-error-message="Image is required"
+                  />
+                </div>
 
-              <input onClick={handleSubmit} type="submit" value="Create" />
-            </form>
+                <input onClick={handleSubmit} type="submit" value="Create" />
+              </form>
+            ) : (
+              <div className="text-center mt-5">
+                <Spinner
+                  className="m-auto text-center"
+                  animation="border"
+                  role="status"
+                >
+                  <span className="visually-hidden">Loading...</span>
+                </Spinner>
+              </div>
+            )}
           </div>
         </div>
-      </>
+      </div>
     );
   }
 };
