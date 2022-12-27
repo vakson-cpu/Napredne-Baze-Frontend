8c589b7252a1a0e8bc473854ad0b92726929a693
vakson-cpu <vakson12@gmail.com>
2022-12-27 15:44:22 +0100

    Create Components for display feeding grounds animals.

---
+++ src/Components/FeedingGrounds/AnimalTable.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import { Spinner } from "react-bootstrap";
+
+import Table from "react-bootstrap/Table";
+const AnimalTable = ({ Animals }) => {
+  return (
+    <Table variant="dark" className="text-center" striped bordered hover size="sm" responsive={true}>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Latin Name</th>
+          <th>LocalName Name</th>
+          <th>First appeared</th>
+        </tr>
+        {Animals.map((item, index) => (
+          <tr key={index}>
+            <td>{index + 1}</td>
+            <td>{item.animal.latinName}</td>
+            <td>{item.animal.localName}</td>
+            <td>{item.firstSeen.slice(0,10)}</td>
+          </tr>
+        ))}
+      </thead>
+      <tbody></tbody>
+    </Table>
+  );
+};
+export default AnimalTable;
