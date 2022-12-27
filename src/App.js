22196f6b338d2097edac4b61373d31a140d55b37
vakson-cpu <vakson12@gmail.com>
2022-12-27 12:04:05 +0100

    Add new routes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
-import React, { useState, useEffect } from "react";
-import { Button } from "react-bootstrap";
-import RegionCard from "./Components/RegionCard/RegionCard";
+import React, { useEffect } from "react";
+
 import Info from "./Pages/InfoPage/Info";
-import Region from "./Pages/RegionPage/Region";
+import Region from "./Pages/Regions/RegionPage/Region";
 import Footer from "./Shared/Footer/Footer";
 import Navbar from "./Shared/Navbar/Navbar";
 import axios from "axios";
 import { Routes, Route } from "react-router-dom";
-import { FileUpload } from "./FileUpload";
-import { PlantSrevice } from "./Services/PlantService";
-import ImageUpload from "./Shared/ImageUpload/ImageUpload";
-import CreatePlant from "./Components/Plants/CreatePlantsForm/CreatePlant";
+import CreatePlant from './Components/Plants/CreatePlantsForm/CreatePlant'
+
 import { GetRegions } from "./Redux/RegionSlice";
 import { useDispatch } from "react-redux";
+import RegionInfoPage from "./Pages/Regions/RegionInfo/RegionInfoPage";
+import FeedingGrounds from "./Pages/FeedingGrounds/FeedingGrounds";
+import AnimalsInFeedingGround from "./Pages/FeedingGrounds/AnimalsInFeedingGround";
 
 const App = () => {
-  const [selectedFile, setSelectedFile] = useState("");
   const dispatch = useDispatch();
-  const apiCall = async () => {
-    let result = await PlantSrevice.CreatePlant(selectedFile);
-    console.log(result);
-  };
-  function handleChange(e) {
-    setSelectedFile(e.target.files[0]);
-  }
+
+
   useEffect(() => {
     dispatch(GetRegions());
   }, [dispatch]);
@@ -31,19 +25,14 @@ const App = () => {
   return (
     <>
       <Navbar></Navbar>
-      {/* <input type='file'  onChange={(e)=>setSelectedFile(e.target.files[0])}   />
-      <button onClick={apiCall}>VAHAA</button>
-      <br>
-      </br>
-      <h1>
-        First
-      </h1>
-
       <Routes>
         <Route path="/" element={<Info />}></Route>
-        <Route path="/Regions" element={<Region />} />
-      </Routes> */}
-      <CreatePlant />
+        <Route path="Regions" element={<Region />} />
+        <Route path="Regions/:regionId" element={<RegionInfoPage />} />
+        <Route path='Plants/Create' element={<CreatePlant />} />
+        <Route path='FeedingGrounds/:regionId' element={<FeedingGrounds/>} />
+        <Route path='FeedingGrounds/:regionId/FgId/:id/Animals' element={<AnimalsInFeedingGround/>} />
+      </Routes>
       <Footer></Footer>
     </>
   );
