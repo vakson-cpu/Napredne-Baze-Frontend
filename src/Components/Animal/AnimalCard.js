289913f787de37f552097e3456b2f984eb9635c0
vakson-cpu <vakson12@gmail.com>
2022-12-27 12:13:43 +0100

    Create Animal Card component

---
+++ src/Components/Animal/AnimalCard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Card, Badge } from 'react-bootstrap'
+const AnimalCard = ({title,LocalName,img,Villages}) => {
+  
+  return (
+    <Card className='mt-3' style={{ width: "18rem",marginLeft:"30px" }}>
+    <Card.Img
+      variant="top"
+      style={{width:"100%", height:"200px"}}
+      src={`https://localhost:5001/api/Plants/image/${img}`}
+    />
+    <Card.Body>
+      <Card.Title>{title}</Card.Title>
+      <Card.Text>Local Name: {LocalName}</Card.Text>
+      <Card.Text>Villages:{Villages} </Card.Text>
+    </Card.Body>
+  </Card>
+  )
+}
+
+export default AnimalCard
