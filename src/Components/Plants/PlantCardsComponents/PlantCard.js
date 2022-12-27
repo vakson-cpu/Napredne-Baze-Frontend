b5bd0065e80b55e34dc642a490223888420cc31d
vakson-cpu <vakson12@gmail.com>
2022-12-27 15:45:06 +0100

    Create PlantCard and implement necesssary logic and design

---
+++ src/Components/Plants/PlantCardsComponents/PlantCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Card, Button, Badge } from "react-bootstrap";
+import { Protections } from "../../../Enums/ProtectionsEnum";
+const PlantCard = ({ title, LocalName, Rarity, img }) => {
+  var obj = { rarity: "", label: "" };
+  switch (Rarity) {
+    case Protections.COMMON:
+      obj.label = "Common";
+      obj.rarity = "success";
+      break;
+    case Protections.RARE:
+      obj.label = "Rare";
+      obj.rarity = "warning";
+      break;
+    case Protections.ENDANGERED:
+      obj.label = "Endangered";
+      obj.rarity = "danger";
+      break;
+
+    default:
+      break;
+  }
+
+  return (
+    <Card className='mt-3' style={{ width: "18rem",marginLeft:"30px" }}>
+      <Card.Img
+        variant="top"
+        style={{width:"100%", height:"200px"}}
+        src={`https://localhost:5001/api/Plants/image/${img}`}
+      />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>Local Name: {LocalName}</Card.Text>
+        <Card.Text>Rarity: </Card.Text>
+        <Badge bg={obj.rarity}>{obj.label}</Badge>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default PlantCard;
