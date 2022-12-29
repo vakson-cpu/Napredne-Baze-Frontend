cf8fc41983c965e21afe37e0aa072403c114caec
vakson-cpu <vakson12@gmail.com>
2022-12-30 00:13:50 +0100

    Configure login modal

--- src/Shared/Modals/LoginModal.js
+++ src/Shared/Modals/LoginModal.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { Button, Modal } from "react-bootstrap";
 import { UserService } from "../../Services/UserService";
-import { useSelector,useDispatch } from "react-redux";
-import {LogInUser} from '../../Redux/AuthSlice'
+import { useSelector, useDispatch } from "react-redux";
+import { LogInUser } from "../../Redux/AuthSlice";
 import jwtDecode from "jwt-decode";
 
 import "./LoginModal.css";
+import { Roles } from "../../Enums/RoleEnum";
 export default function LoginModal({ handleShow, show }) {
   const [switchSign, setSwitchSign] = useState(true); //Initially set on true for log-in
-  const dispatch= useDispatch();
+  const dispatch = useDispatch();
   const [Name, setName] = useState("");
   const [LastName, setLastName] = useState("");
   const [Password, setPassword] = useState("");
@@ -16,26 +17,24 @@ export default function LoginModal({ handleShow, show }) {
   const [Email, setEmail] = useState("");
 
   const handleSubmit = async () => {
-    if (switchSign === false){
+    if (switchSign === false) {
       await UserService.Register({ Name, LastName, Email, Password });
       setSwitchSign(true);
-    }
-    else {
-      
+    } else {
       // let response=await UserService.LogIn({ Email, Password });
-      let response=await dispatch(LogInUser({Email, Password }))
+      let response = await dispatch(LogInUser({ Email, Password }));
       console.log(response.payload);
-      if(response.payload.succeeded){
-      let user=jwtDecode(response.payload.data);
-      console.log(user)
-      localStorage.setItem("token",response.data)
-      localStorage.setItem("Name",user.Name);
-      localStorage.setItem("LastName",user.LastName);
-      localStorage.setItem("Role",user.Role);
-      localStorage.setItem("Id",user.id);
-      handleShow(false);
+      if (response.payload.succeeded) {
+        let user = jwtDecode(response.payload.data);
+        console.log(user);
+        // let role= Object.getOwnPropertyDescriptor(Roles,user.Role).value;
+        localStorage.setItem("token", response.payload.data);
+        localStorage.setItem("Name", user.Name);
+        localStorage.setItem("LastName", user.LastName);
+        // localStorage.setItem("Role", role);
+        localStorage.setItem("Id", user.Id);
+        handleShow(false);
       }
-    
     }
   };
 
