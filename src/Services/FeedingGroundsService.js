b284cae9caa4f7e03e48a195ba70302e268cf254
vakson-cpu <vakson12@gmail.com>
2022-12-30 00:13:18 +0100

    Configure FeedingGroundsService

--- src/Services/FeedingGroundsService.js
+++ src/Services/FeedingGroundsService.js
@@ -4,8 +4,8 @@ const baseURL = "http://localhost:5000/api";
 axios.defaults.baseURL = baseURL;
 
 export  const FeedingGroundsService = {
-  GetByRegionId: async(id) =>
-    await axios.get(`FeedingGrounds/Get/Region/${id}`).then((res) =>res.data ).catch((err)=>err),
+  GetByRegionId: async(id,pageNumber) =>
+    await axios.get(`FeedingGrounds/Get/Region/${id}?pageNumber=${pageNumber}`).then((res) =>res.data ).catch((err)=>err),
   GetFeedingGroundById:async(id)=>
   await axios.get(`FeedingGrounds/Get/${id}`).then(res=>res.data).catch(err=>err)
 
