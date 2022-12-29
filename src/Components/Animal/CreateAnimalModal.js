a8b769935711c424b20386e59a6dbf8fa5fcc825
vakson-cpu <vakson12@gmail.com>
2022-12-30 00:11:17 +0100

    Create Modal for creating animals

--- src/Components/Animal/CreateAnimalModal.js
+++ src/Components/Animal/CreateAnimalModal.js
@@ -1,42 +1,89 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import "./ListOfAnimals.css";
+export default function CreateAnimalModal({ show, onClose }) {
+  const ImageRef = useRef();
+  const [file, setFile] = useState("");
+  const [url, setUrl] = useState("");
+  const [LocalName, setLocalName] = useState("");
+  const [LatinName, setLatinName] = useState("");
+  const [Villages, setVillages] = useState("");
+  useEffect(() => {
+    if (!file) return;
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setUrl(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  }, [file]);
 
-export function CreateAnimalModal({ show, onClose }) {
-  const ImageRef= useRef();
+  const handlePickImage = (e) => {
+    ImageRef.current.click();
+  };
+  console.log(Villages)
   return (
     <Modal
       show={show}
-      size="lg"
+      size="md"
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
+      <Modal.Header className="bg-dark text-white">
+        <Modal.Title id="contained-modal-title-vcenter bg-dark">
           Create Animal
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body className="bg-black">
-        <div className="d-flex flex-row">
-          <label className="text-white">Latin Name</label>
-          <input type="text" />
+      <Modal.Body className="bg-dark  d-flex flex-column justify-content-center ">
+        <div className="mt-4 d-flex flex-row  m-auto ">
+          <label className="text-white mr-1 ">Latin Name: </label>
+          <input
+            value={LatinName}
+            onChange={(e) => setLatinName(e.target.value)}
+            type="text"
+            className="input-modal"
+          />
         </div>
-        <div className="d-flex flex-row">
-          <label className="text-white">Local Name</label>
-          <input type="text" />
+        <div className="mt-4 d-flex flex-row m-auto ">
+          <label className="text-white mr-1">Local Name: </label>
+          <input
+            value={LocalName}
+            onChange={(e) => setLocalName(e.target.value)}
+            type="text"
+            className="input-modal"
+          />
         </div>
-        <div className="d-flex flex-row">
-          <label className="text-white">Villages</label>
-          <input type="text" />
+        <div className=" mt-4 d-flex flex-row  m-auto">
+          <label className="text-white">Villages: </label>
+          <input
+            value={Villages}
+            onChange={(e) => setVillages(e.target.value)}
+            type="text"
+            className="input-modal"
+          />
         </div>
-        <div className="d-flex flex-row">
-          <label className="text-white">Image</label>
-          <Button variant='outline-warning' >Add Image</Button>
-          <input  ref={ImageRef} className='visually-hidden' type="file" />
+        <div className="mt-4 d-flex flex-row m-auto ">
+          <Button onClick={() => handlePickImage()} variant="outline-warning">
+            Add Image
+          </Button>
+          <input
+            ref={ImageRef}
+            onChange={(e) => setFile(e.target.files[0])}
+            className="visually-hidden"
+            type="file"
+          />
+        </div>{" "}
+        <div>
+          {url ? (
+            <img className="image-box-preview-animal" src={url} alt="nece" />
+          ) : (
+            "Preview..."
+          )}
         </div>
       </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={onClose}>Close</Button>
+      <Modal.Footer className='bg-dark'>
+        <Button variant='outline-danger' onClick={onClose}>Close</Button>
+        <Button variant='outline-warning' onClick={onClose}>Submit</Button>
       </Modal.Footer>
     </Modal>
   );
