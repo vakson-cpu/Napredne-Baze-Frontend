810dc02e8e5cc1decb2a10ae9d26d9f49fb63b4f
vakson-cpu <vakson12@gmail.com>
2022-12-31 02:07:12 +0100

    Implement logic for create animal modal, add full functionality

--- src/Components/Animal/CreateAnimalModal.js
+++ src/Components/Animal/CreateAnimalModal.js
@@ -2,13 +2,40 @@ import React, { useState, useRef, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import "./ListOfAnimals.css";
-export default function CreateAnimalModal({ show, onClose }) {
+import { useDispatch, useSelector } from "react-redux";
+import { Col, Row, Spinner } from "react-bootstrap";
+import { AnimalService } from "../../Services/AnimalService";
+import { GetRegions } from "../../Redux/RegionSlice";
+export default function CreateAnimalModal({
+  show,
+  onClose,
+  setShow1,
+  setShow2,
+}) {
   const ImageRef = useRef();
   const [file, setFile] = useState("");
   const [url, setUrl] = useState("");
   const [LocalName, setLocalName] = useState("");
   const [LatinName, setLatinName] = useState("");
   const [Villages, setVillages] = useState("");
+  const [isLoading, setIsLoadng] = useState(false);
+  // const [regions, setRegions] = useState([]);
+
+  const regions = useSelector((state) => state.RegionSlice.regions);
+
+  const dispatch = useDispatch();
+
+  const [checkList, setCheckList] = useState([]);
+
+  const handleChecked = (e) => {
+    let helpArray = checkList.filter((item) => item === e.target.value);
+    if (helpArray.length === 0) setCheckList([...checkList, e.target.value]);
+    else {
+      helpArray = checkList.filter((item) => item !== e.target.value);
+      setCheckList([...helpArray]);
+    }
+  };
+
   useEffect(() => {
     if (!file) return;
     const fileReader = new FileReader();
@@ -21,70 +48,155 @@ export default function CreateAnimalModal({ show, onClose }) {
   const handlePickImage = (e) => {
     ImageRef.current.click();
   };
-  console.log(Villages)
+
+  const handleSubmit = async () => {
+    // setIsLoadng(true);
+    let token = localStorage.getItem("token");
+    let response = await AnimalService.InsertAnimal(
+      {
+        LatinName: LatinName,
+        LocalName: LocalName,
+        Villages: Villages,
+        file: file,
+        Regions: checkList,
+      },
+      token
+    );
+    console.log(response);
+    if (response !== undefined) {
+      if (response.succeeded === true) {
+        dispatch(GetRegions());
+        setIsLoadng(false);
+        setShow1(true);
+        onClose(false);
+        return;
+      }
+      else {
+        setShow2(true);
+        onClose(false);
+      }
+    } else {
+      setShow2(true);
+      onClose(false);
+    }
+  };
+
   return (
-    <Modal
-      show={show}
-      size="md"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header className="bg-dark text-white">
-        <Modal.Title id="contained-modal-title-vcenter bg-dark">
-          Create Animal
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body className="bg-dark  d-flex flex-column justify-content-center ">
-        <div className="mt-4 d-flex flex-row  m-auto ">
-          <label className="text-white mr-1 ">Latin Name: </label>
-          <input
-            value={LatinName}
-            onChange={(e) => setLatinName(e.target.value)}
-            type="text"
-            className="input-modal"
-          />
-        </div>
-        <div className="mt-4 d-flex flex-row m-auto ">
-          <label className="text-white mr-1">Local Name: </label>
-          <input
-            value={LocalName}
-            onChange={(e) => setLocalName(e.target.value)}
-            type="text"
-            className="input-modal"
-          />
-        </div>
-        <div className=" mt-4 d-flex flex-row  m-auto">
-          <label className="text-white">Villages: </label>
-          <input
-            value={Villages}
-            onChange={(e) => setVillages(e.target.value)}
-            type="text"
-            className="input-modal"
-          />
-        </div>
-        <div className="mt-4 d-flex flex-row m-auto ">
-          <Button onClick={() => handlePickImage()} variant="outline-warning">
-            Add Image
+    <div>
+      <Modal
+        show={show}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header className="bg-dark text-white">
+          <Modal.Title id="contained-modal-title-vcenter bg-dark">
+            Create Animal
+          </Modal.Title>
+        </Modal.Header>
+        {isLoading === false ? (
+          <Modal.Body className="bg-dark  d-flex flex-column justify-content-center ">
+            <Row className="m-auto mb-4 mt-4">
+              <Col>
+                {" "}
+                <label className="text-white mr-1  " style={{ width: "110px" }}>
+                  Latin Name:{" "}
+                </label>
+              </Col>
+              <Col>
+                <input
+                  value={LatinName}
+                  onChange={(e) => setLatinName(e.target.value)}
+                  type="text"
+                  className="input-modal"
+                />
+              </Col>
+            </Row>
+            <Row className="m-auto">
+              <Col className="text-white" style={{ width: "110px" }}>
+                Local Name:{" "}
+              </Col>
+              <Col>
+                <input
+                  value={LocalName}
+                  onChange={(e) => setLocalName(e.target.value)}
+                  type="text"
+                  className="input-modal"
+                />
+              </Col>
+            </Row>
+            <Row className=" mt-4 d-flex flex-row  m-auto">
+              <Col>
+                <label className="text-white" style={{ width: "110px" }}>
+                  Villages:{" "}
+                </label>
+              </Col>
+              <Col>
+                <input
+                  value={Villages}
+                  onChange={(e) => setVillages(e.target.value)}
+                  type="text"
+                  className="input-modal"
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col className="mt-5  text-center m-auto ">
+                <label className="text-white fs-5">Pick a Region</label>
+                {regions.map((item) => {
+                  return (
+                    <div className="regions-box-animal">
+                      <label className="label-checkbox">{item.name}</label>
+                      <input
+                        type="checkbox"
+                        value={item.id}
+                        onChange={handleChecked}
+                      />
+                    </div>
+                  );
+                })}
+              </Col>
+            </Row>
+            <div className="mt-4 d-flex flex-row m-auto ">
+              <Button
+                onClick={() => handlePickImage()}
+                variant="outline-warning"
+              >
+                Add Image
+              </Button>
+              <input
+                ref={ImageRef}
+                onChange={(e) => setFile(e.target.files[0])}
+                className="visually-hidden"
+                type="file"
+              />
+            </div>{" "}
+            <div>
+              {url ? (
+                <img
+                  className="image-box-preview-animal"
+                  src={url}
+                  alt="nece"
+                />
+              ) : (
+                "Preview..."
+              )}
+            </div>
+          </Modal.Body>
+        ) : (
+          <Modal.Body>
+            <p className="text-white text-center">"Loading..."</p>
+          </Modal.Body>
+        )}
+        <Modal.Footer className="bg-dark">
+          <Button variant="outline-danger" onClick={() => onClose(false)}>
+            Close
           </Button>
-          <input
-            ref={ImageRef}
-            onChange={(e) => setFile(e.target.files[0])}
-            className="visually-hidden"
-            type="file"
-          />
-        </div>{" "}
-        <div>
-          {url ? (
-            <img className="image-box-preview-animal" src={url} alt="nece" />
-          ) : (
-            "Preview..."
-          )}
-        </div>
-      </Modal.Body>
-      <Modal.Footer className='bg-dark'>
-        <Button variant='outline-danger' onClick={onClose}>Close</Button>
-        <Button variant='outline-warning' onClick={onClose}>Submit</Button>
-      </Modal.Footer>
-    </Modal>
+          <Button variant="outline-warning" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
   );
 }
