c343fc133c37c849e343448240d207d745e637ca
vakson-cpu <vakson12@gmail.com>
2022-12-31 02:17:51 +0100

    Add checks if localstorage contains token, fill redux.

--- src/App.js
+++ src/App.js
@@ -6,32 +6,45 @@ import Footer from "./Shared/Footer/Footer";
 import Navbar from "./Shared/Navbar/Navbar";
 import axios from "axios";
 import { Routes, Route } from "react-router-dom";
-import CreatePlant from './Components/Plants/CreatePlantsForm/CreatePlant'
+import CreatePlant from "./Components/Plants/CreatePlantsForm/CreatePlant";
 
 import { GetRegions } from "./Redux/RegionSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import RegionInfoPage from "./Pages/Regions/RegionInfo/RegionInfoPage";
 import FeedingGrounds from "./Pages/FeedingGrounds/FeedingGrounds";
 import AnimalsInFeedingGround from "./Pages/FeedingGrounds/AnimalsInFeedingGround";
+import CreateAnimalModal from "./Components/Animal/CreateAnimalModal";
+import { signIn } from "./Redux/AuthSlice";
 
 const App = () => {
   const dispatch = useDispatch();
 
-
   useEffect(() => {
     dispatch(GetRegions());
   }, [dispatch]);
+  useEffect(() => {
+    if (localStorage.length > 0) {
+      let role = localStorage.getItem("Role");
+      dispatch(signIn(role));
+    }
+  }, []);
 
   return (
     <>
       <Navbar></Navbar>
       <Routes>
-        <Route path="/" element={<Info />}></Route>
+        <Route path="/" element={<Info  />}></Route>
         <Route path="Regions" element={<Region />} />
         <Route path="Regions/:regionId" element={<RegionInfoPage />} />
-        <Route path='Plants/Create' element={<CreatePlant />} />
-        <Route path='FeedingGrounds/:regionId' element={<FeedingGrounds/>} />
-        <Route path='FeedingGrounds/:regionId/FgId/:id/Animals' element={<AnimalsInFeedingGround/>} />
+        <Route path="Plants/Create" element={<CreatePlant />} />
+        <Route
+          path="FeedingGrounds/:regionId/:pageNumber"
+          element={<FeedingGrounds />}
+        />
+        <Route
+          path="FeedingGrounds/:regionId/FgId/:id/Animals"
+          element={<AnimalsInFeedingGround />}
+        />
       </Routes>
       <Footer></Footer>
     </>
