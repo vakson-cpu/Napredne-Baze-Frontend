34e13bf8c7cadc1ba0eb5a8b51f0d054821cb775
vakson-cpu <vakson12@gmail.com>
2022-12-31 02:19:40 +0100

    Add insert

---
+++ src/Services/AnimalService.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+const baseURL = "http://localhost:5000/api";
+
+axios.defaults.baseURL = baseURL;
+
+export const AnimalService = {
+  InsertAnimal: async (animal, token) => {
+    console.log(animal);
+    let bodyFormData = new FormData();
+    bodyFormData.append("LatinName", animal.LatinName);
+    bodyFormData.append("LocalName", animal.LocalName);
+    bodyFormData.append("Villages", animal.Villages);
+    for (let i = 0; i < animal.Regions.length; i++)
+      bodyFormData.append("Regions", animal.Regions[i]);
+    bodyFormData.append("file", animal.file);
+    let res = axios
+      .post("Animal/AddAnimal", bodyFormData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        return res.data;
+      })
+      .catch((res) => res);
+    return res;
+  },
+};
+export default AnimalService;
