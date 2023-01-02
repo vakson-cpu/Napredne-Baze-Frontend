feb2752f6ee067df3eef63520023473e6fb6fcb2
vakson-cpu <vakson12@gmail.com>
2023-01-02 01:18:12 +0100

    Add servie for adding animal to feeding ground, in FeedingGroundsService file

--- src/Services/FeedingGroundsService.js
+++ src/Services/FeedingGroundsService.js
@@ -7,7 +7,11 @@ export  const FeedingGroundsService = {
   GetByRegionId: async(id,pageNumber) =>
     await axios.get(`FeedingGrounds/Get/Region/${id}?pageNumber=${pageNumber}`).then((res) =>res.data ).catch((err)=>err),
   GetFeedingGroundById:async(id)=>
-  await axios.get(`FeedingGrounds/Get/${id}`).then(res=>res.data).catch(err=>err)
+  await axios.get(`FeedingGrounds/Get/${id}`).then(res=>res.data).catch(err=>err),
+  AddAnimalToFeedingGrounds:async(animalId,feedingGroundId,firstSeen)=>{
+    await axios.put("FeedingGrounds/AddAnimal",{animalId:animalId,feedingGroundId:feedingGroundId,firstSeen:firstSeen}).then(res=>res.data).catch(err=>err);
+  }
+
 
 };
 export default FeedingGroundsService;
