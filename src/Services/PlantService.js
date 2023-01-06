2a16587d7aec043960e7a92470d72dd0dbfc143e
vakson-cpu <vakson12@gmail.com>
2023-01-06 23:41:44 +0100

    Configure getall method add sort query

--- src/Services/PlantService.js
+++ src/Services/PlantService.js
@@ -27,8 +27,8 @@ export const PlantSrevice = {
       .catch((res) => res);
     return res;
   },
-  GetAllPlants:async (plantNumber)=>{
-    let result = axios.get(`Plants/Getall?pageNumber=${plantNumber}`).then(res=>res.data).catch(err=>err);
+  GetAllPlants:async (pageNumber,SortBy,sortValue)=>{
+    let result = axios.get(`Plants/Getall?pageNumber=${pageNumber}&SortBy=${SortBy}&sortValue=${sortValue}`).then(res=>res.data).catch(err=>err);
     return result;
   }
 };
