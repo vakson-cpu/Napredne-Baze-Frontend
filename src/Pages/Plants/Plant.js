5717f4ee71fa0e7cabd63cfee96685f33a6ff84e
vakson-cpu <vakson12@gmail.com>
2023-01-06 23:41:14 +0100

    Add search functionality

--- src/Pages/Plants/Plant.js
+++ src/Pages/Plants/Plant.js
@@ -4,25 +4,30 @@ import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import PlantCard from "../../Components/Plants/PlantCardsComponents/PlantCard";
 import { Spinner } from "react-bootstrap";
-import './Plant.css'
+import Search from "../../Shared/Search/Search";
+import {Button} from "react-bootstrap"
+import "./Plant.css";
 
 const Plant = () => {
   let id = +useParams().pageNumber;
   const [plants, setPlants] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [sortBy, setSortBy] = useState("");
+  const [sortValue, setsortValue] = useState("");
   const [currentPage, setCurrentPage] = useState(id ?? 1);
   const [PagginationArray, setPagginationArray] = useState([]);
 
   const getAll = async (something) => {
     console.log(something);
-    setCurrentPage(something)
+    setCurrentPage(something);
     setLoading(true);
     let result = await PlantSrevice.GetAllPlants(something);
-    console.log(result)
+    console.log(result);
     if (result.succeeded === true) {
       setPlants(result.data.plants);
       let helpArray = [];
-      for (let i = 0; i < Math.ceil(result.data.count/8); i++) helpArray.push(i+1);
+      for (let i = 0; i < Math.ceil(result.data.count / 8); i++)
+        helpArray.push(i + 1);
       setPagginationArray(helpArray);
     }
     setLoading(false);
@@ -36,29 +41,51 @@ const Plant = () => {
     getAll(item);
   };
 
+  const handleSearch=async()=>{
+    setLoading(true)
+    let result = await PlantSrevice.GetAllAnimals("1",sortBy,sortValue);
+    setPlants(result.data.plants);
+    setLoading(false)
+  }
   console.log(plants);
   if (loading === false)
     return (
       <div className="parentWrapper">
-        <h1 className='mt-5 mb-2 text-success text-center '>All plants located on Golija</h1>
-        { plants.length>0 ?(
-        <div className="d-flex flex-row flex-wrap justify-content-center mt-5">
-          {plants.map((item) => (
-            <PlantCard
-              LocalName={item.localName}
-              title={item.latinName}
-              img={item.image}
-              Rarity={item.protectionId}
-            />
-          ))}
-        </div>):<div className="mt-5 mb-5"><h2 className="text-center text-success">There is no plants in this region</h2></div>
-          }
-        <div className="childWrapper d-flex d-row flex-wrap mt-2 mb-5" style={{ marginLeft: "150px" }}>
+        <h1 className="mt-5 mb-2 text-success text-center ">
+          All plants located on Golija
+        </h1>
+        <div className="m-auto mt-5 text-center" style={{ width: "300px" }}>
+          {" "}
+          <Search  setSort={setSortBy} setSortValue={setsortValue} sortBy={sortBy} sortValue={sortValue} />
+          <Button onClick={handleSearch} className='mt-2 m-auto text-center ' variant='success' >Search</Button>
+
+        </div>
+        {plants.length > 0 ? (
+          <div className="d-flex flex-row flex-wrap justify-content-center mt-5">
+            {plants.map((item) => (
+              <PlantCard
+                LocalName={item.localName}
+                title={item.latinName}
+                img={item.image}
+                Rarity={item.protectionId}
+              />
+            ))}
+          </div>
+        ) : (
+          <div className="mt-5 mb-5">
+            <h2 className="text-center text-success">
+              There is no plants in this region
+            </h2>
+          </div>
+        )}
+        <div
+          className="childWrapper d-flex d-row flex-wrap mt-2 mb-5"
+          style={{ marginLeft: "150px" }}
+        >
           {PagginationArray.map((item) => {
             return (
               <Link className="pagi-Link" to={`/Plants/Get/${item}`}>
                 <span
-             
                   className={
                     currentPage === item ? "pagi-item active" : "pagi-item"
                   }
@@ -71,17 +98,18 @@ const Plant = () => {
         </div>
       </div>
     );
-  else return (
-    <div className="text-center mt-5">
-      <Spinner
-        className="m-auto text-center"
-        animation="border"
-        role="status"
-      >
-        <span className="visually-hidden">Loading...</span>
-      </Spinner>
-    </div>
-  );
+  else
+    return (
+      <div className="text-center mt-5">
+        <Spinner
+          className="m-auto text-center"
+          animation="border"
+          role="status"
+        >
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </div>
+    );
 };
 
 export default Plant;
