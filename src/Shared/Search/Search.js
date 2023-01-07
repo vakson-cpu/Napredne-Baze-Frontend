b8377927492f6ee30bcf551b5e0a8cba7b58afb2
vakson-cpu <vakson12@gmail.com>
2023-01-07 02:14:03 +0100

    Add conditional rendering for plant search

--- src/Shared/Search/Search.js
+++ src/Shared/Search/Search.js
@@ -2,21 +2,43 @@ import React from "react";
 import { Form } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearchengin } from "@fortawesome/free-brands-svg-icons";
+import { Protections } from "../../Enums/ProtectionsEnum";
 
-const Search = ({setSort,setSortValue,sortBy,sortValue}) => {
-    const LOCAL_NAME="LocalName";
-    const LATIN_NAME="LatinName";
-  console.log("sb ",sortBy)
+const Search = ({
+  setSort,
+  setSortValue,
+  sortBy,
+  sortValue,
+  IsPlants,
+  rarityValue,
+  setRarityValue,
+}) => {
+  const LOCAL_NAME = "LocalName";
+  const LATIN_NAME = "LatinName";
+  const Rarity = [
+    {
+      Rarity: "Common",
+      value: Protections.COMMON,
+    },
+    {
+      Rarity: "Rare",
+      value: Protections.RARE,
+    },
+    {
+      Rarity: "Endangered",
+      value: Protections.ENDANGERED,
+    },
+  ];
+  console.log("rarity",rarityValue);
   return (
     <div className="text-center">
       <div style={{ display: "flex", flexDirection: "row" }}>
         <Form.Control
-          
           style={{ width: "260px" }}
           type="text"
           placeholder={"Search"}
           value={sortValue}
-          onChange={(e)=>setSortValue(e.target.value)}
+          onChange={(e) => setSortValue(e.target.value)}
         />
         <FontAwesomeIcon
           style={{ cursor: "pointer" }}
@@ -33,6 +55,16 @@ const Search = ({setSort,setSortValue,sortBy,sortValue}) => {
         <option>{LATIN_NAME}</option>
         <option>{LOCAL_NAME}</option>
       </Form.Select>
+      { IsPlants &&
+      <Form.Select
+        value={rarityValue}
+        onChange={(e) => setRarityValue(e.target.value)}
+        className="mt-2"
+        aria-label="Search By"
+      >
+        {Rarity.map(item=><option value={item.value}>{item.Rarity}</option>)}
+      </Form.Select>
+}
     </div>
   );
 };
