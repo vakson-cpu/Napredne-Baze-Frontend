e18ebe1fae98dd66f11a7d8fa2814573a87bb34e
vakson-cpu <vakson12@gmail.com>
2023-01-10 23:39:57 +0100

    Create User page

---
+++ src/Pages/Admin/Users.js
@@ -0,0 +1,32 @@
+import React,{useEffect,useState} from 'react'
+import UserDetails from '../../Components/Users/UserDetails';
+import { UserService } from '../../Services/UserService';
+import './Users.css'
+const Users = () => {
+  const [Users, setUsers] = useState([]);
+  const [Loading, setLoading] = useState(true)
+  useEffect(() => {
+    const handleFetching=async()=>{
+      let result = await UserService.GetAllUsers();
+      setUsers(result.data);
+      setLoading(false)
+    }
+    handleFetching();
+  }, [])
+  
+  return (
+    <div>
+        
+      <h1 className='text-center '>Insights into all users of the app</h1>
+
+      {
+        Loading===false? <div   className='bg-light User-Table '> <UserDetails Users={Users} /></div>: 
+        <div>Loading...</div>
+      
+      }
+        
+    </div>
+  )
+}
+
+export default Users
