5a103f351c9d13fd8adbb368950a9cccc2c5bd1e
vakson-cpu <vakson12@gmail.com>
2023-01-11 23:55:43 +0100

    Configure user details

--- src/Components/Users/UserDetails.js
+++ src/Components/Users/UserDetails.js
@@ -1,62 +1,92 @@
-import React from "react";
-import { Table,Badge, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Table, Badge, Button } from "react-bootstrap";
 import { Roles } from "../../Enums/RoleEnum";
+import ConfigureUserModal from "./ConfigureUserModal";
 const UserDetails = ({ Users }) => {
-
+  const [show, setShow] = useState(false);
+  const [userId, setUserId] = useState("");
+  const [roleId, setRoleId] = useState("");
+  const handleButtonClick = (valueForUserId, valueForRole) => {
+    setUserId(valueForUserId);
+    setRoleId(valueForRole);
+    setShow(!show);
+  };
   const handleRole = (role) => {
     switch (role) {
       case 1:
-        return Roles.Administrator
+        return Roles.Administrator;
       case 2:
-        return Roles.Worker
+        return Roles.Worker;
       case 3:
-        return Roles.User
+        return Roles.User;
       default:
         return Roles.Guest;
     }
   };
-  const handleBadgeColor=(role)=>{
+  const handleBadgeColor = (role) => {
     switch (role) {
       case 1:
-        return "danger"
+        return "danger";
       case 2:
-        return "warning"
+        return "warning";
       case 3:
-        return 'success'
+        return "success";
       default:
-        return 'white';
+        return "white";
     }
-  }
+  };
   return (
-    <Table
-      variant="light"
-      className="text-center"
-      striped
-      bordered
-      hover
-      size="sm"
-      responsive={true}
-    >
-      <thead>
-        <tr className="text-center">
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Role</th>
-          <th>Adjust</th>
-        </tr>
-        {Users.map((item, index) => (
-          <tr key={index} className="text-center" >
-            <td>{index + 1}</td>
-            <td>{item.name}</td>
-            <td>{item.lastName}</td>
-            <td>{<Badge bg={handleBadgeColor(item.roleId)}  >{handleRole(item.roleId)}</Badge>}</td>
-            <td ><Button className='m-auto' variant="outline-dark" >Adjust Role</Button></td>
+    <>
+      <Table
+        variant="light"
+        className="text-center"
+        striped
+        bordered
+        hover
+        size="sm"
+        responsive={true}
+      >
+        <thead>
+          <tr className="text-center">
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Role</th>
+            <th>Adjust</th>
           </tr>
-        ))}
-      </thead>
-      <tbody></tbody>
-    </Table>
+          {Users.map((item, index) => (
+            <tr key={index} className="text-center">
+              <td>{index + 1}</td>
+              <td>{item.name}</td>
+              <td>{item.lastName}</td>
+              <td>
+                {
+                  <Badge bg={handleBadgeColor(item.roleId)}>
+                    {handleRole(item.roleId)}
+                  </Badge>
+                }
+              </td>
+              <td>
+                <Button
+                  className="m-auto"
+                  variant="outline-dark"
+                  onClick={() => handleButtonClick(item.id, item.roleId)}
+                >
+                  Adjust Role
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </thead>
+      </Table>
+
+      <ConfigureUserModal
+        show={show}
+        onClose={setShow}
+        userId={userId}
+        roleId={roleId}
+      />
+    </>
   );
 };
 
