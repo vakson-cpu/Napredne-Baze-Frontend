b7bccaeb74458e8138eea9a4ae19c63bd30ffceb
vakson-cpu <vakson12@gmail.com>
2023-01-20 23:08:02 +0100

    Create ManageWorkers page

---
+++ src/Pages/Admin/ManageWorkers.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FeedingGroundsTable from '../../Components/FeedingGrounds/FeedingGroundsTable'
+
+const ManageWorkers = () => {
+  return (
+    <div>
+      <FeedingGroundsTable />
+    </div>
+  )
+}
+
+export default ManageWorkers
