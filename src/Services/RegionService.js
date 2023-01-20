15c68a5f4bf1d4e4e0b108befdbb52170cd64e94
vakson-cpu <vakson12@gmail.com>
2023-01-20 23:06:32 +0100

    Add updateWorkers region service

--- src/Services/RegionService.js
+++ src/Services/RegionService.js
@@ -3,8 +3,19 @@ const baseURL = "http://localhost:5000/api";
 
 axios.defaults.baseURL = baseURL;
 
-export  const RegionService = {
-  GetRegions: async() =>
-     axios.get("Regions/GetAll").then((res) =>res.data ).catch((err)=>err)
-
+export const RegionService = {
+  GetRegions: async () =>
+    axios
+      .get("Regions/GetAll")
+      .then((res) => res.data)
+      .catch((err) => err),
+  UpdateWorker: async (workerId, regionId, salary) =>
+    axios
+      .put("Regions/UpdateWorker", {
+        userId: workerId,
+        Salary: salary,
+        regionid: regionId,
+      })
+      .then((res) => res.data)
+      .catch((err) => err),
 };
