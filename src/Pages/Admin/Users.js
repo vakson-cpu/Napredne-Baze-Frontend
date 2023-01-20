77709a6b16d20eb52f714e8354dfa808495bca6b
vakson-cpu <vakson12@gmail.com>
2023-01-20 23:04:44 +0100

    onfigure loadign spinners

--- src/Pages/Admin/Users.js
+++ src/Pages/Admin/Users.js
@@ -1,32 +1,44 @@
-import React,{useEffect,useState} from 'react'
-import UserDetails from '../../Components/Users/UserDetails';
-import { UserService } from '../../Services/UserService';
-import './Users.css'
+import React, { useEffect, useState } from "react";
+import UserDetails from "../../Components/Users/UserDetails";
+import { UserService } from "../../Services/UserService";
+import { Spinner } from "react-bootstrap";
+import "./Users.css";
 const Users = () => {
   const [Users, setUsers] = useState([]);
-  const [Loading, setLoading] = useState(true)
+  const [Loading, setLoading] = useState(true);
   useEffect(() => {
-    const handleFetching=async()=>{
+    const handleFetching = async () => {
       let result = await UserService.GetAllUsers();
       setUsers(result.data);
-      setLoading(false)
-    }
+      setLoading(false);
+    };
     handleFetching();
-  }, [])
-  
+  }, []);
+
   return (
     <div>
-        
-      <h1 className='text-center text-success fw-bold'>Insights into all users of the app</h1>
+      <h1 className="text-center text-success fw-bold">
+        Insights into all users of the app
+      </h1>
 
-      {
-        Loading===false? <div   className='bg-light User-Table mt-5 mb-5 '> <UserDetails Users={Users} setUsers={setUsers} /></div>: 
-        <div>Loading...</div>
-      
-      }
-        
+      {Loading === false ? (
+        <div className="bg-light User-Table mt-5 mb-5 ">
+          {" "}
+          <UserDetails Users={Users} setUsers={setUsers} />
+        </div>
+      ) : (
+        <div className="text-center" style={{marginTop:"100px"}}>
+          <Spinner
+            className="m-auto text-center"
+            animation="border"
+            role="status"
+          >
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Users
+export default Users;
