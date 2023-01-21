a46c4740fd52008cb8434a021d9142171748b9b4
vakson-cpu <vakson12@gmail.com>
2023-01-21 23:39:40 +0100

    Configure footer content add icons

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./Footer.css";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {  faFacebookF , } from '@fortawesome/free-brands-svg-icons';
+import {
+  faFacebookF,
+  faInstagram,
+  faSnapchat,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 import {
   solid,
   regular,
@@ -15,30 +20,16 @@ const Footer = () => {
       <div className="footer-container">
         <div className="row justify-content-center">
           <div className=" col-sm-4 col-md-3 item">
-            <h3 className="text-success ">Services</h3>
+            <h3 className="text-success ">Data</h3>
             <ul className="Link">
               <li>
-                <Link to="#">Web design</Link>
+                <Link to="#">Natural Parks</Link>
               </li>
               <li>
-                <Link to="#">Development</Link>
+                <Link to="#">Documents</Link>
               </li>
               <li>
-                <Link to="#">Hosting</Link>
-              </li>
-            </ul>
-          </div>
-          <div className="col-sm-4 col-md-3 item">
-            <h3 className="text-success">About</h3>
-            <ul className="Link">
-              <li>
-                <Link to="#">Company</Link>
-              </li>
-              <li>
-                <Link to="#">Team</Link>
-              </li>
-              <li>
-                <Link to="#">Legacy</Link>
+                <Link to="#">Other</Link>
               </li>
             </ul>
           </div>
@@ -47,12 +38,12 @@ const Footer = () => {
             <ul className="Link">
               <li>
                 <Link className="Link" to="#">
-                  Job openings
+                  Apply for working
                 </Link>
               </li>
               <li>
                 <Link className="Link" to="#">
-                  Employee success
+                  Daily life as a worker
                 </Link>
               </li>
               <li>
@@ -60,19 +51,36 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div className="col-lg-3 item social">
+          <div className="col-sm-4 col-md-3 item">
+            <h3 className="text-success">Security</h3>
+            <ul className="Link">
+              <li>
+                <Link to="#">Info</Link>
+              </li>
+              <li>
+                <Link to="#">Q&A</Link>
+              </li>
+              <li>
+                <Link to="#">Help</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="col-lg-3 item social text-dark">
             <Link to="#">
-              <i className="icon ion-social-facebook "></i>
+              <FontAwesomeIcon icon={faSnapchat} />{" "}
             </Link>
             <Link to="#">
-              <i className="icon ion-social-twitter"></i>
+              <FontAwesomeIcon icon={faLinkedin} />{" "}
             </Link>
+
             <Link to="#">
-              <i className="icon ion-social-snapchat"></i>
+              <FontAwesomeIcon icon={faInstagram} />{" "}
             </Link>
+
             <Link to="#">
-            <FontAwesomeIcon icon={faFacebookF} />        </Link>   
-             <p className="copyright">Company Name © 2018</p>
+              <FontAwesomeIcon icon={faFacebookF} />{" "}
+            </Link>
+            <p className="copyright">Company Name © 2018</p>
           </div>
         </div>
       </div>
