13428cc1e32005b3ae2ec84a1a2970cdaf1ec950
vakson-cpu <vakson12@gmail.com>
2023-01-23 19:16:57 +0100

    Add WorkSlice

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import AuthSlice from '../Redux/AuthSlice'
+import WorkerSlice from './WorkerSlice'
 import RegionSlice from './RegionSlice'
 export const store = configureStore({
   reducer: {
     AuthSlice,
-    RegionSlice
+    RegionSlice,
+    WorkerSlice
   },
 })
