2ee3bc1275517b7cbac1b644a209fe1879f66ae4
vakson-cpu <vakson12@gmail.com>
2023-01-23 19:11:05 +0100

    Make necessary changes in  ListOfAnimals components

--- src/Components/Animal/ListOfAnimals.js
+++ src/Components/Animal/ListOfAnimals.js
@@ -4,43 +4,49 @@ import "./ListOfAnimals.css";
 import { useSelector } from "react-redux";
 import AnimalCard from "./AnimalCard";
 import { STATUS } from "../../Enums/StatusEnum";
-const ListOfAnimals = ({ Animals }) => {
+const ListOfAnimals = ({ regionId }) => {
   const [List, setList] = useState([]);
   const [PagginationArray, setPagginationArray] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const status = useSelector((state) => state.RegionSlice.status);
-
+  const regions = useSelector((state) => state.RegionSlice.regions);
   useEffect(() => {
-    console.log("ANIMALS ", Animals);
+    const region = regions.filter((item) => item.id === regionId);
+    let animals = region[0].animals;
     const DummyArray = [];
-    for (let i = 1; i <= Math.ceil(Animals.length / 8); i++) {
+    for (let i = 1; i <= Math.ceil(animals.length / 8); i++) {
       DummyArray.push(i);
     }
     setPagginationArray(DummyArray);
   }, [status]);
 
   useEffect(() => {
+    const region = regions.filter((item) => item.id === regionId);
+    let animals = region[0].animals;
     let DummyArray = [];
     let animalDummz = [];
-    for (let i = 1; i <= Math.ceil(Animals.length / 8); i++) {
+    for (let i = 1; i <= Math.ceil(animals.length / 8); i++) {
       DummyArray.push(i);
     }
-    if (Animals.length < 8)
-      for (let i = 0; i < Animals.length; i++)
-        animalDummz.push(Animals[i].animal);
-    else for (let i = 0; i < 8; i++) animalDummz.push(Animals[i].animal);
+    if (animals.length < 8)
+      for (let i = 0; i < animals.length; i++)
+        animalDummz.push(animals[i].animal);
+    else for (let i = 0; i < 8; i++) animalDummz.push(animals[i].animal);
 
     setList(animalDummz);
   }, [status]);
+
   const handlePaginate = (item) => {
+    const region = regions.filter((item) => item.id === regionId);
+    let animals = region[0].animals;
     setCurrentPage(item);
     let dummyAr = [];
     if (item === PagginationArray.length) {
-      for (let i = (item - 1) * 8; i < Animals.length; i++)
-        dummyAr.push(Animals[i].animal);
+      for (let i = (item - 1) * 8; i < animals.length; i++)
+        dummyAr.push(animals[i].animal);
     } else
       for (let i = (item - 1) * 8; i < (item - 1) * 8 + 8; i++) {
-        dummyAr.push(Animals[i].animal);
+        dummyAr.push(animals[i].animal);
       }
     setList(dummyAr);
     console.log("tu displej", dummyAr);
@@ -51,7 +57,7 @@ const ListOfAnimals = ({ Animals }) => {
         <div className="d-flex flex-row flex-wrap">
           {List.map((item) => (
             <AnimalCard
-            key={item.id}
+              key={item.id}
               LocalName={item.localName}
               title={item.latinName}
               img={item.image}
